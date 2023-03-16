9dc6615491cbba97ddad20c9f8d8856bcc510d4a
mbaluyot <m.gwynethbaluyot@gmail.com>
2023-03-16 14:54:07 +0800

    initial commit

---
+++ src/pages/SharedLayout.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import StyledNavbar from '../components/StyledNavbar';
+import Footer from '../components/Footer';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <StyledNavbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+export default SharedLayout;
